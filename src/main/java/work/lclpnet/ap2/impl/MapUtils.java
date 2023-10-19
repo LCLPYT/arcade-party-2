355980e51223832cd49d56144eb7a6866f95bcd5
LCLP <lukas@lclpnet.work>
2023-10-19 16:12:48 +0200

    improve WorldFacadeImpl#changeMap

--- src/main/java/work/lclpnet/ap2/impl/MapUtils.java
+++ src/main/java/work/lclpnet/ap2/impl/MapUtils.java
@@ -1,7 +1,6 @@
 package work.lclpnet.ap2.impl;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
@@ -13,15 +12,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static work.lclpnet.lobby.config.ConfigUtil.getVec3d;
-
 @Prototype
 public class MapUtils {
 
     @NotNull
     public static Vec3d getSpawnPosition(GameMap gameMap) {
         if (gameMap.getProperty("spawn") instanceof JSONArray array) {
-            return getVec3d(array);
+            return getSpawnVec3d(array);
         }
 
         throw missingProperty("spawn");
@@ -37,7 +34,7 @@ public class MapUtils {
 
         for (Object element : array) {
             if (element instanceof JSONArray elemArray) {
-                spawns.add(getVec3d(elemArray));
+                spawns.add(getSpawnVec3d(elemArray));
             }
         }
 
@@ -56,11 +53,35 @@ public class MapUtils {
             Object value = object.get(key);
 
             if (value instanceof JSONArray elemArray) {
-                spawns.put(key, getVec3d(elemArray));
+                spawns.put(key, getSpawnVec3d(elemArray));
             }
         }
 
         return spawns;
+    }
+
+    private static Vec3d getSpawnVec3d(JSONArray json) {
+        if (json.length() < 3) {
+            throw new IllegalArgumentException("JSONArray must have at least 3 elements");
+        }
+
+        return new Vec3d(
+                getSpawnDouble(json, 0),
+                json.getDouble(1),  // do not center y
+                getSpawnDouble(json, 2)
+        );
+    }
+
+    private static double getSpawnDouble(JSONArray json, int index) {
+        double d = json.getDouble(index);
+        int i = json.getInt(index);
+
+        if (Math.abs(d - i) < 1e-6) {
+            // return centered block pos
+            return i + 0.5;
+        }
+
+        return d;
     }
 
     private static IllegalStateException missingProperty(String property) {
