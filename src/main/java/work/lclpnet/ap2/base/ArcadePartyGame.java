a24b733dd888a0a1fb9e628cc705647d20addce7
LCLP <lukas@lclpnet.work>
2023-10-20 02:59:42 +0200

    implement GameInstance

--- src/main/java/work/lclpnet/ap2/base/ArcadePartyGame.java
+++ src/main/java/work/lclpnet/ap2/base/ArcadePartyGame.java
@@ -1,43 +1,19 @@
 package work.lclpnet.ap2.base;
 
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.MinecraftServer;
-import org.slf4j.Logger;
-import work.lclpnet.activity.manager.ActivityManager;
-import work.lclpnet.ap2.api.WorldFacade;
-import work.lclpnet.ap2.base.activity.PreparationActivity;
-import work.lclpnet.ap2.impl.WorldFacadeImpl;
 import work.lclpnet.lobby.game.Game;
 import work.lclpnet.lobby.game.GameEnvironment;
+import work.lclpnet.lobby.game.GameInstance;
 import work.lclpnet.lobby.game.conf.GameConfig;
 import work.lclpnet.lobby.game.conf.MinecraftGameConfig;
-import work.lclpnet.lobby.game.map.MapCollection;
-import work.lclpnet.lobby.game.map.MapManager;
-import work.lclpnet.lobby.game.map.MapRepository;
-import work.lclpnet.lobby.game.map.UrlMapRepository;
-import work.lclpnet.lobby.util.WorldContainer;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.util.concurrent.CompletableFuture;
 
 public class ArcadePartyGame implements Game {
 
-    private final Logger logger = ArcadeParty.logger;
     private final MinecraftGameConfig config;
-    private final MapManager mapManager;
 
     public ArcadePartyGame() {
         config = new MinecraftGameConfig("ap2", "Arcade Party", new ItemStack(Items.GOLD_BLOCK));
-
-        try {
-            this.mapManager = createMapManager();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Failed to setup map repository url", e);
-        }
     }
 
     @Override
@@ -46,53 +22,7 @@ public class ArcadePartyGame implements Game {
     }
 
     @Override
-    public boolean canStart(GameEnvironment environment) {
-        if (ApConstants.DEVELOPMENT) {
-            return true;
-        }
-
-        MinecraftServer server = environment.getServer();
-
-        return PlayerLookup.all(server).size() >= 2;
-    }
-
-    @Override
-    public void start(GameEnvironment environment) {
-        MinecraftServer server = environment.getServer();
-        ArcadeParty arcadeParty = ArcadeParty.getInstance();
-
-        WorldContainer worldContainer = new WorldContainer(server);
-        arcadeParty.registerUnloadable(worldContainer);
-
-        WorldFacadeImpl worldFacade = new WorldFacadeImpl(server, mapManager);
-        worldFacade.init(arcadeParty);  // TODO replace with game lifecycle hook registrar
-
-        PreparationActivity preparation = new PreparationActivity(arcadeParty, worldFacade);
-
-        load().thenRun(() ->
-                        server.submit(() ->
-                                ActivityManager.getInstance().startActivity(preparation)))
-                .exceptionally(throwable -> {
-                    logger.error("Failed to load ArcadeParty2", throwable);
-                    return null;
-                });
-    }
-
-    private MapManager createMapManager() throws MalformedURLException {
-        MapRepository mapRepository = new UrlMapRepository(Path.of("worlds").toUri().toURL(), logger);
-
-        return new MapManager(mapRepository, logger);
-    }
-
-    private CompletableFuture<Void> load() {
-        return CompletableFuture.runAsync(() -> {
-            MapCollection mapCollection = mapManager.getMapCollection();
-
-            try {
-                mapCollection.load(ApConstants.ID);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to load maps of namespace %s".formatted(ApConstants.ID), e);
-            }
-        });
+    public GameInstance createInstance(GameEnvironment gameEnvironment) {
+        return new ArcadePartyGameInstance(gameEnvironment);
     }
 }
---
+++ src/main/java/work/lclpnet/ap2/base/ArcadePartyGameInstance.java
@@ -0,0 +1,97 @@
+package work.lclpnet.ap2.base;
+
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import work.lclpnet.activity.manager.ActivityManager;
+import work.lclpnet.ap2.base.activity.PreparationActivity;
+import work.lclpnet.ap2.impl.WorldFacadeImpl;
+import work.lclpnet.kibu.plugin.hook.HookStack;
+import work.lclpnet.lobby.game.GameEnvironment;
+import work.lclpnet.lobby.game.GameInstance;
+import work.lclpnet.lobby.game.GameStarter;
+import work.lclpnet.lobby.game.map.MapCollection;
+import work.lclpnet.lobby.game.map.MapManager;
+import work.lclpnet.lobby.game.map.MapRepository;
+import work.lclpnet.lobby.game.map.UrlMapRepository;
+import work.lclpnet.lobby.game.start.ConditionGameStarter;
+import work.lclpnet.lobby.util.WorldContainer;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+
+public class ArcadePartyGameInstance implements GameInstance {
+
+    private final Logger logger = ArcadeParty.logger;
+    private final GameEnvironment environment;
+    private final MapManager mapManager;
+
+    public ArcadePartyGameInstance(GameEnvironment environment) {
+        this.environment = environment;
+
+        try {
+            this.mapManager = createMapManager();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Failed to setup map repository url", e);
+        }
+    }
+
+    @Override
+    public GameStarter createStarter(GameStarter.Args args, GameStarter.Callback callback) {
+        return new ConditionGameStarter(this::canStart, args, callback, environment);
+    }
+
+    private boolean canStart() {
+        if (ApConstants.DEVELOPMENT) {
+            return true;
+        }
+
+        MinecraftServer server = environment.getServer();
+
+        return PlayerLookup.all(server).size() >= 2;
+    }
+
+    @Override
+    public void start() {
+        MinecraftServer server = environment.getServer();
+        ArcadeParty arcadeParty = ArcadeParty.getInstance();
+
+        WorldContainer worldContainer = new WorldContainer(server);
+        arcadeParty.registerUnloadable(worldContainer);
+
+        HookStack hookStack = environment.getHookStack();
+
+        WorldFacadeImpl worldFacade = new WorldFacadeImpl(server, mapManager);
+        worldFacade.init(hookStack);
+
+        PreparationActivity preparation = new PreparationActivity(arcadeParty, worldFacade);
+
+        load().thenRun(() ->
+                        server.submit(() ->
+                                ActivityManager.getInstance().startActivity(preparation)))
+                .exceptionally(throwable -> {
+                    logger.error("Failed to load ArcadeParty2", throwable);
+                    return null;
+                });
+    }
+
+    private CompletableFuture<Void> load() {
+        return CompletableFuture.runAsync(() -> {
+            MapCollection mapCollection = mapManager.getMapCollection();
+
+            try {
+                mapCollection.load(ApConstants.ID);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load maps of namespace %s".formatted(ApConstants.ID), e);
+            }
+        });
+    }
+
+    private MapManager createMapManager() throws MalformedURLException {
+        MapRepository mapRepository = new UrlMapRepository(Path.of("worlds").toUri().toURL(), logger);
+
+        return new MapManager(mapRepository, logger);
+    }
+}
