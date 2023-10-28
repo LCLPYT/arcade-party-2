848a11c1ae7b08e2af124b2460e8efc36ffae36f
LCLP <lukas@lclpnet.work>
2023-10-28 02:08:50 +0200

    migrate to mg-lobby 0.24.0

--- src/main/java/work/lclpnet/ap2/api/base/MiniGameManager.java
+++ src/main/java/work/lclpnet/ap2/api/base/MiniGameManager.java
@@ -2,7 +2,9 @@ package work.lclpnet.ap2.api.base;
 
 import net.minecraft.util.Identifier;
 import work.lclpnet.ap2.api.game.MiniGame;
+import work.lclpnet.ap2.game.MiniGames;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -11,4 +13,11 @@ public interface MiniGameManager {
     Set<MiniGame> getGames();
 
     Optional<MiniGame> getGame(Identifier gameId);
+
+    static Set<MiniGame> getAllGames() {
+        HashSet<MiniGame> games = new HashSet<>();
+        MiniGames.registerGames(games);
+
+        return games;
+    }
 }
