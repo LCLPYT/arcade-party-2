e8dec50497e199c440133bad5e7e26d9f568ef7b
tilln <tillnachtigall2@gmail.com>
2023-11-01 22:53:06 +0100

    Added placeChest method in TreasureHunterInstance.

--- src/main/java/work/lclpnet/ap2/game/treasure_hunter/TreasureHunterInstance.java
+++ src/main/java/work/lclpnet/ap2/game/treasure_hunter/TreasureHunterInstance.java
@@ -1,27 +1,25 @@
 package work.lclpnet.ap2.game.treasure_hunter;
 
-import mglobby.org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Formatting;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import work.lclpnet.ap2.api.game.MiniGameHandle;
 import work.lclpnet.ap2.api.game.data.DataContainer;
 import work.lclpnet.ap2.impl.game.DefaultGameInstance;
 import work.lclpnet.kibu.access.entity.PlayerInventoryAccess;
 import work.lclpnet.kibu.inv.item.ItemStackUtil;
-import work.lclpnet.kibu.translate.TranslationService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class TreasureHunterInstance extends DefaultGameInstance {
 
     private final BlockPos mapCenter = new BlockPos(0,64,0); //add Y from center Variable in map.json
-    private final Integer maxRadius = 20; //add value from map.json
     private final Integer mapDepth = 8;
 
     public TreasureHunterInstance(MiniGameHandle gameHandle) {
@@ -61,23 +59,24 @@ public class TreasureHunterInstance extends DefaultGameInstance {
     private void placeChest() {
 
         int depth = surfaceDepth();
-        final BlockPos corner1 = new BlockPos(mapCenter.getX()+maxRadius,mapCenter.getY()-mapDepth, mapCenter.getZ()-maxRadius);
-        final BlockPos corner2 = new BlockPos(mapCenter.getX()-maxRadius,mapCenter.getY()-depth, mapCenter.getZ()+maxRadius);
-        Iterable<BlockPos> chestArea = BlockPos.iterate(corner1,corner2);
-        int areaSize = (maxRadius*2)*(maxRadius*2)*(mapDepth-depth);
+        ServerWorld world = getWorld(); //add value from map.json
+        int maxRadius = 20;
+        final BlockPos corner1 = new BlockPos(mapCenter.getX()+ maxRadius,mapCenter.getY()-mapDepth, mapCenter.getZ()- maxRadius);
+        final BlockPos corner2 = new BlockPos(mapCenter.getX()- maxRadius,mapCenter.getY()-depth, mapCenter.getZ()+ maxRadius);
+        List<BlockPos> chestAreaList = new ArrayList<>();
         Random rand = new Random();
-        int chestPos = rand.nextInt(areaSize);
-        int i = 0;
 
-        for (BlockPos block : chestArea) {
-            if (!getWorld().getBlockState(block).isOf(Blocks.SAND)) {continue;}
-            if (i==chestPos) {}//place chest, return
-            i+=1;
+        for (BlockPos block : BlockPos.iterate(corner1,corner2)) {
+            if (world.getBlockState(block).isOf(Blocks.SAND)) {
+                chestAreaList.add(block);
+            }
         }
+        BlockPos chestPos = chestAreaList.get(rand.nextInt(chestAreaList.size()));
+        world.setBlockState(chestPos, Blocks.CHEST.getDefaultState());
     }
 
     private int surfaceDepth() {
-        if (mapDepth-2<=0) {return 1;};
+        if (mapDepth-2<=0) {return 1;}
         return 2;
     }
 }
