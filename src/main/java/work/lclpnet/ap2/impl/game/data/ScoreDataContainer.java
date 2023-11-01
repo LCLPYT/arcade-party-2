93863ceb41ffddc32fa235f578dc8d47e15b401d
till-nachtigall <116350989+till-nachtigall@users.noreply.github.com>
2023-11-01 22:00:21 +0100

    further developed ScoreDataContainer

---
+++ src/main/java/work/lclpnet/ap2/impl/game/data/ScoreDataContainer.java
@@ -0,0 +1,61 @@
+package work.lclpnet.ap2.impl.game.data;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.apache.logging.log4j.core.jmx.Server;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import work.lclpnet.ap2.api.game.data.DataContainer;
+import work.lclpnet.ap2.api.game.data.DataEntry;
+import work.lclpnet.ap2.api.game.data.PlayerRef;
+import work.lclpnet.ap2.impl.game.data.entry.ScoreDataEntry;
+import work.lclpnet.ap2.impl.game.data.entry.SimpleDataEntry;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class ScoreDataContainer implements DataContainer {
+
+    private final HashMap<PlayerRef, Integer> scoreMap = new HashMap<>();
+
+    public void setScore(ServerPlayerEntity player, int score) {
+        synchronized (this) {
+            scoreMap.put(PlayerRef.create(player), score);
+        }
+    }
+
+    public void addScore(ServerPlayerEntity player, int add) {
+        synchronized (this) {
+            PlayerRef key = PlayerRef.create(player);
+            scoreMap.put(key, scoreMap.computeIfAbsent(key, playerRef -> 0) + add);
+        }
+    }
+
+    public int getScore(ServerPlayerEntity player) {
+        synchronized (this) {
+            return scoreMap.computeIfAbsent(PlayerRef.create(player), playerRef -> 0);
+        }
+    }
+
+    @Override
+    public void delete(ServerPlayerEntity player) {
+        synchronized (this) {
+            scoreMap.remove(PlayerRef.create(player));
+        }
+    }
+
+    @Override
+    public DataEntry getEntry(ServerPlayerEntity player) {
+        PlayerRef ref = PlayerRef.create(player);
+        synchronized (this) {
+            return new ScoreDataEntry(ref, scoreMap.get(ref));
+        }
+    }
+
+    @Override
+    public Stream<? extends DataEntry> orderedEntries() {
+        return scoreMap.entrySet().stream()
+                .map(playerRefIntegerEntry -> new ScoreDataEntry(playerRefIntegerEntry.getKey(), playerRefIntegerEntry.getValue()))
+                .sorted(Comparator.comparingInt(ScoreDataEntry::score));
+    }
+}
