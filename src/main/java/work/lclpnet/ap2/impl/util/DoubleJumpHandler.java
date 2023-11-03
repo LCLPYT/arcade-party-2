ad2f2631540d3d07c2ee28e7f86170a24f2854ad
LCLP <lukas@lclpnet.work>
2023-11-03 01:58:15 +0100

    add double jump cooldown

--- src/main/java/work/lclpnet/ap2/impl/util/DoubleJumpHandler.java
+++ src/main/java/work/lclpnet/ap2/impl/util/DoubleJumpHandler.java
@@ -1,4 +1,4 @@
-package work.lclpnet.ap2.impl;
+package work.lclpnet.ap2.impl.util;
 
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
@@ -8,14 +8,26 @@ import work.lclpnet.ap2.impl.game.PlayerUtil;
 import work.lclpnet.kibu.access.VelocityModifier;
 import work.lclpnet.kibu.hook.player.PlayerToggleFlightCallback;
 import work.lclpnet.kibu.plugin.hook.HookRegistrar;
+import work.lclpnet.kibu.scheduler.Ticks;
+import work.lclpnet.kibu.scheduler.api.TaskScheduler;
 
 public class DoubleJumpHandler {
 
     private final PlayerUtil playerUtil;
+    private final Cooldown cooldown;
+    private int cooldownTicks = Ticks.seconds(2);
     private boolean enabled = false;
 
-    public DoubleJumpHandler(PlayerUtil playerUtil) {
+    public DoubleJumpHandler(PlayerUtil playerUtil, TaskScheduler scheduler) {
         this.playerUtil = playerUtil;
+        this.cooldown = new Cooldown(scheduler);
+
+        cooldown.setOnCooldownOver(player -> {
+            if (!enabled) return;
+
+            player.getAbilities().allowFlying = true;
+            player.sendAbilitiesUpdate();
+        });
     }
 
     public boolean isEnabled() {
@@ -28,11 +40,29 @@ public class DoubleJumpHandler {
         this.enabled = enabled;
 
         playerUtil.setAllowFlight(enabled);
+        cooldown.resetAll();
+    }
+
+    public int getCooldownTicks() {
+        return cooldownTicks;
+    }
+
+    public void setCooldownTicks(int cooldownTicks) {
+        this.cooldownTicks = cooldownTicks;
     }
 
     public void init(HookRegistrar hookRegistrar) {
         hookRegistrar.registerHook(PlayerToggleFlightCallback.HOOK, (player, fly) -> {
             if (!fly) return false;
+
+            if (cooldown.isOnCooldown(player)) {
+                return true;
+            }
+
+            cooldown.setCooldown(player, cooldownTicks);
+
+            player.getAbilities().allowFlying = false;
+            player.sendAbilitiesUpdate();
 
             VelocityModifier.setVelocity(player, player.getRotationVector().multiply(1.3));
 
