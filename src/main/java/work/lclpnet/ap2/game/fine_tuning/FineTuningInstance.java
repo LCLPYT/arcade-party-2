e414e517855e2e02d6ccc18c9dd6541947f53eb3
LCLP <lukas@lclpnet.work>
2023-11-06 16:50:41 +0100

    teleport player into their own rooms

--- src/main/java/work/lclpnet/ap2/game/fine_tuning/FineTuningInstance.java
+++ src/main/java/work/lclpnet/ap2/game/fine_tuning/FineTuningInstance.java
@@ -4,6 +4,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.GameMode;
 import work.lclpnet.ap2.api.game.MiniGameHandle;
 import work.lclpnet.ap2.api.game.data.DataContainer;
 import work.lclpnet.ap2.api.map.MapFacade;
@@ -26,9 +28,13 @@ import java.util.concurrent.CompletableFuture;
 public class FineTuningInstance extends DefaultGameInstance {
 
     private final ScoreDataContainer data = new ScoreDataContainer();
+    private BlockPos baseSpawn;
+    private Vec3i roomOffset;
 
     public FineTuningInstance(MiniGameHandle gameHandle) {
         super(gameHandle);
+
+        setDefaultGameMode(GameMode.SURVIVAL);  // survival is needed to left-click note blocks
     }
 
     @Override
@@ -46,7 +52,7 @@ public class FineTuningInstance extends DefaultGameInstance {
 
     @Override
     protected void prepare() {
-
+        teleportParticipants();
     }
 
     @Override
@@ -89,7 +95,8 @@ public class FineTuningInstance extends DefaultGameInstance {
 
     private void placeStructures(GameMap map, ServerWorld world, BlockStructure structure) {
         BlockPos roomStart = MapUtil.readBlockPos(map.requireProperty("room-start"));
-        BlockPos roomDirection = normalize(MapUtil.readBlockPos(map.requireProperty("room-direction")));
+        Vec3i roomDirection = normalize(MapUtil.readBlockPos(map.requireProperty("room-direction")));
+        Vec3i spawnOffset = MapUtil.readBlockPos(map.requireProperty("room-player-spawn"));
 
         final int rooms = gameHandle.getParticipants().getAsSet().size();
         final int width = structure.getWidth(),
@@ -97,14 +104,27 @@ public class FineTuningInstance extends DefaultGameInstance {
                 length = structure.getWidth();
 
         int rz = roomDirection.getZ(), rx = roomDirection.getX(), ry = roomDirection.getY();
+        int sx = roomStart.getX(), sy = roomStart.getY(), sz = roomStart.getZ();
+
+        baseSpawn = new BlockPos(
+                sx + spawnOffset.getX(),
+                sy + spawnOffset.getY(),
+                sz + spawnOffset.getZ()
+        );
+
+        roomOffset = new Vec3i(
+                rx * (width - Math.abs(rx)),
+                ry * (height - Math.abs(ry)),
+                rz * (length - Math.abs(rz))
+        );
 
         BlockPos.Mutable pos = new BlockPos.Mutable();
 
         for (int i = 0; i < rooms; i++) {
             pos.set(
-                    roomStart.getX() + i * rx * (width - Math.abs(rx)),
-                    roomStart.getY() + i * ry * (height - Math.abs(ry)),
-                    roomStart.getZ() + i * rz * (length - Math.abs(rz))
+                    sx + i * roomOffset.getX(),
+                    sy + i * roomOffset.getY(),
+                    sz + i * roomOffset.getZ()
             );
 
             StructureUtil.placeStructure(structure, world, pos);
@@ -120,5 +140,23 @@ public class FineTuningInstance extends DefaultGameInstance {
                 (y < 0) ? -1 : ((y == 0) ? 0 : 1),
                 (z < 0) ? -1 : ((z == 0) ? 0 : 1)
         );
+    }
+
+    private void teleportParticipants() {
+        ServerWorld world = getWorld();
+        float yaw = MapUtil.readAngle(getMap().requireProperty("room-player-yaw"));
+
+        double sx = baseSpawn.getX() + 0.5, sy = baseSpawn.getY(), sz = baseSpawn.getZ() + 0.5;
+        int ox = roomOffset.getX(), oy = roomOffset.getY(), oz = roomOffset.getZ();
+
+        int i = 0;
+
+        for (ServerPlayerEntity player : gameHandle.getParticipants()) {
+            double x = sx + ox * i, y = sy + oy * i, z = sz + oz * i;
+
+            player.teleport(world, x, y, z, yaw, 0.0F);
+
+            i++;
+        }
     }
 }
