1a3119be9ad616d10949bbd38e4e8a4f6a7cb8b7
LCLP <lukas@lclpnet.work>
2023-11-06 18:52:29 +0100

    play note blocks per player

--- src/main/java/work/lclpnet/ap2/impl/util/SoundHelper.java
+++ src/main/java/work/lclpnet/ap2/impl/util/SoundHelper.java
@@ -1,6 +1,8 @@
 package work.lclpnet.ap2.impl.util;
 
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
@@ -12,6 +14,16 @@ public class SoundHelper {
         for (ServerPlayerEntity player : PlayerLookup.all(server)) {
             player.playSound(sound, category, volume, pitch);
         }
+    }
+
+    public static void playSound(ServerPlayerEntity player, SoundEvent sound, SoundCategory category,
+                                 double x, double y, double z, float volume, float pitch) {
+
+        var entry = Registries.SOUND_EVENT.getEntry(sound);
+        long seed = player.getRandom().nextLong();
+        var packet = new PlaySoundS2CPacket(entry, category, x, y, z, volume, pitch, seed);
+
+        player.networkHandler.sendPacket(packet);
     }
 
     private SoundHelper() {}
