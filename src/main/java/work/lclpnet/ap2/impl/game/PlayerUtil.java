7f3145c2064a99d2166b6b1e62a948a50bf6d1fd
LCLP <lukas@lclpnet.work>
2023-11-24 18:34:13 +0100

    upgrade combat-control version

--- src/main/java/work/lclpnet/ap2/impl/game/PlayerUtil.java
+++ src/main/java/work/lclpnet/ap2/impl/game/PlayerUtil.java
@@ -26,6 +26,7 @@ public class PlayerUtil {
     public static final GameMode INITIAL_GAMEMODE = GameMode.ADVENTURE;
     private final MinecraftServer server;
     private final PlayerManager playerManager;
+    private final CombatControl combatControl;
     private final Set<ApEffect> effects = new ObjectOpenHashSet<>(1);
     private GameMode defaultGameMode = INITIAL_GAMEMODE;
     private CombatStyle defaultCombatStyle = CombatStyle.OLD;
@@ -34,6 +35,7 @@ public class PlayerUtil {
     public PlayerUtil(MinecraftServer server, PlayerManager playerManager) {
         this.server = server;
         this.playerManager = playerManager;
+        this.combatControl = CombatControl.get(server);
     }
 
     public void setDefaultGameMode(@NotNull GameMode defaultGameMode) {
@@ -47,6 +49,7 @@ public class PlayerUtil {
 
     public void setDefaultCombatStyle(CombatStyle defaultCombatStyle) {
         this.defaultCombatStyle = defaultCombatStyle;
+        combatControl.setStyle(this.defaultCombatStyle);
     }
 
     public CombatStyle getDefaultCombatStyle() {
@@ -133,7 +136,7 @@ public class PlayerUtil {
 
         effects.forEach(effect -> effect.apply(player));
 
-        CombatControl.get(server).setStyle(player, defaultCombatStyle);
+        combatControl.setStyle(player, defaultCombatStyle);
     }
 
     public void resetToDefaults() {
