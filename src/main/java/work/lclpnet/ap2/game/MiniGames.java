aa208a6a7c5aa9f9972c9affff988abf03fe8a64
LCLP <lukas@lclpnet.work>
2023-12-16 16:40:00 +0100

    refine treasure hunter mini game

--- src/main/java/work/lclpnet/ap2/game/MiniGames.java
+++ src/main/java/work/lclpnet/ap2/game/MiniGames.java
@@ -7,6 +7,7 @@ import work.lclpnet.ap2.game.fine_tuning.FineTuningMiniGame;
 import work.lclpnet.ap2.game.mirror_hop.MirrorHopMiniGame;
 import work.lclpnet.ap2.game.one_in_the_chamber.OneInTheChamberMiniGame;
 import work.lclpnet.ap2.game.spleef.SpleefMiniGame;
+import work.lclpnet.ap2.game.treasure_hunter.TreasureHunterMinigame;
 
 import java.util.Set;
 
@@ -21,7 +22,7 @@ public class MiniGames {
         games.add(new BowSpleefMiniGame());
         games.add(new MirrorHopMiniGame());
         games.add(new FineTuningMiniGame());
-//        games.add(new TreasureHunterMinigame());
+        games.add(new TreasureHunterMinigame());
         games.add(new OneInTheChamberMiniGame());
         games.add(new AnvilFallMiniGame());
     }
--- src/main/java/work/lclpnet/ap2/game/treasure_hunter/TreasureHunterInstance.java
+++ src/main/java/work/lclpnet/ap2/game/treasure_hunter/TreasureHunterInstance.java
@@ -2,6 +2,7 @@ package work.lclpnet.ap2.game.treasure_hunter;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -15,36 +16,31 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
-import org.json.JSONArray;
 import work.lclpnet.ap2.api.base.Participants;
 import work.lclpnet.ap2.api.game.MiniGameHandle;
 import work.lclpnet.ap2.api.game.data.DataContainer;
 import work.lclpnet.ap2.impl.game.DefaultGameInstance;
 import work.lclpnet.ap2.impl.game.data.OrderedDataContainer;
 import work.lclpnet.ap2.impl.map.MapUtil;
+import work.lclpnet.ap2.impl.util.BlockBox;
 import work.lclpnet.kibu.access.entity.PlayerInventoryAccess;
 import work.lclpnet.kibu.hook.entity.PlayerInteractionHooks;
 import work.lclpnet.kibu.inv.item.ItemStackUtil;
 import work.lclpnet.kibu.plugin.hook.HookRegistrar;
 import work.lclpnet.lobby.game.impl.prot.ProtectionTypes;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 public class TreasureHunterInstance extends DefaultGameInstance {
-    Random rand = new Random();
+
+    private final Random rand = new Random();
     private final OrderedDataContainer data = new OrderedDataContainer();
+    private final Set<BlockState> materials = new HashSet<>();
 
     public TreasureHunterInstance(MiniGameHandle gameHandle) {
         super(gameHandle);
 
         setDefaultGameMode(GameMode.SURVIVAL);
-    }
-
-    @Override
-    public void participantRemoved(ServerPlayerEntity player) {
     }
 
     @Override
@@ -54,59 +50,40 @@ public class TreasureHunterInstance extends DefaultGameInstance {
 
     @Override
     protected void prepare() {
+        MapUtil.readBlockStates(getMap().requireProperty("materials"), materials, gameHandle.getLogger());
 
         Participants participants = gameHandle.getParticipants();
 
         HookRegistrar hooks = gameHandle.getHookRegistrar();
 
         hooks.registerHook(PlayerInteractionHooks.USE_BLOCK, (player, world, hand, hitResult) -> {
-
-            if (!(player instanceof ServerPlayerEntity serverPlayer)) return ActionResult.PASS;
-            if (!participants.isParticipating(serverPlayer)) return ActionResult.PASS;
-
-            if (!world.getBlockState(hitResult.getBlockPos()).isOf(Blocks.CHEST)) return ActionResult.PASS;
-
-            player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.BLOCKS, 1.2f, 1.8f);
-            player.playSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 0.2f, 0.5f);
-
-            data.add(serverPlayer);
-            win(serverPlayer);
-            return ActionResult.success(true);
-        });
-
-        hooks.registerHook(PlayerInteractionHooks.ATTACK_BLOCK, (player, world, hand, pos, direction) -> {
-
-            if (!(player instanceof ServerPlayerEntity serverPlayer)) return ActionResult.PASS;
-            if (!participants.isParticipating(serverPlayer)) return ActionResult.PASS;
-
-            if (!world.getBlockState(pos).isOf(Blocks.CHEST)) return ActionResult.PASS;
+            if (!(player instanceof ServerPlayerEntity serverPlayer) || !participants.isParticipating(serverPlayer)
+                || !world.getBlockState(hitResult.getBlockPos()).isOf(Blocks.CHEST) || isGameOver()) {
+                return ActionResult.PASS;
+            }
 
             player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.BLOCKS, 1.2f, 1.8f);
             player.playSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 0.2f, 0.5f);
 
             data.add(serverPlayer);
             win(serverPlayer);
+
             return ActionResult.success(true);
         });
 
         hooks.registerHook(PlayerInteractionHooks.BREAK_BLOCK, (world, player, pos, state, blockEntity) -> {
+            if (!(player instanceof ServerPlayerEntity serverPlayer) || !participants.isParticipating(serverPlayer)) {
+                return true;
+            }
 
-            if (!(player instanceof ServerPlayerEntity serverPlayer)) return true;
-            if (!participants.isParticipating(serverPlayer)) return true;
+            if (!materials.contains(state) || !(rand.nextFloat() < 0.005)) return true;
 
-            if (!state.isOf(Blocks.SAND)) return true;
-            if (!(rand.nextFloat() < 0.005)) return true;
             spawnCoin(pos, world);
+
             return true;
         });
 
         placeChest();
-    }
-
-    private void spawnCoin(BlockPos pos, World world) {
-        ItemEntity coin = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.SUNFLOWER));
-        world.spawnEntity(coin);
-        System.out.println("LCLPCoin spawned at: " + pos);
     }
 
     @Override
@@ -116,13 +93,15 @@ public class TreasureHunterInstance extends DefaultGameInstance {
                 World world = entity.getWorld();
                 BlockState state = world.getBlockState(pos);
 
-                return state.isOf(Blocks.SAND);
+                return materials.contains(state);
             });
+
             config.allow(ProtectionTypes.PICKUP_ITEM, (playerEntity, itemEntity) -> {
                 if (itemEntity.getStack().isOf(Items.SUNFLOWER)) {
                     itemEntity.discard();
                     giveCoin(playerEntity);
                 }
+
                 return false;
             });
         });
@@ -130,7 +109,13 @@ public class TreasureHunterInstance extends DefaultGameInstance {
         giveShovelsToPlayers();
     }
 
+    private void spawnCoin(BlockPos pos, World world) {
+        ItemEntity coin = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.SUNFLOWER));
+        world.spawnEntity(coin);
+    }
+
     private void giveCoin(PlayerEntity player) {
+        // TODO actually give the player the coin
         player.playSound(SoundEvents.ENTITY_ARROW_HIT_PLAYER, SoundCategory.BLOCKS, 0.7f, 1.55f);
     }
 
@@ -138,6 +123,7 @@ public class TreasureHunterInstance extends DefaultGameInstance {
         for (ServerPlayerEntity player : gameHandle.getParticipants()) {
             ItemStack stack = new ItemStack(Items.IRON_SHOVEL);
 
+            stack.addEnchantment(Enchantments.EFFICIENCY, 4);
             ItemStackUtil.setUnbreakable(stack, true);
 
             PlayerInventory inventory = player.getInventory();
@@ -147,23 +133,21 @@ public class TreasureHunterInstance extends DefaultGameInstance {
     }
 
     private void placeChest() {
-
         ServerWorld world = getWorld();
-        final JSONArray chestArea = Objects.requireNonNull(getMap().getProperty("chest-area"), "Chest area not defined.");
-        final BlockPos corner1 = MapUtil.readBlockPos((JSONArray) chestArea.get(0));
-        final BlockPos corner2 = MapUtil.readBlockPos((JSONArray) chestArea.get(1));
+        BlockBox box = MapUtil.readBox(getMap().requireProperty("chest-area"));
         List<BlockPos> chestAreaList = new ArrayList<>();
 
-        for (BlockPos block : BlockPos.iterate(corner1, corner2)) {
-            if (world.getBlockState(block).isOf(Blocks.SAND)) {
+        for (BlockPos block : box) {
+            BlockState state = world.getBlockState(block);
+
+            if (materials.contains(state)) {
                 chestAreaList.add(block.toImmutable());
             }
         }
 
-        int randomIndex = rand.nextInt(1, chestAreaList.size() - 1);
+        int randomIndex = rand.nextInt(chestAreaList.size());
 
         BlockPos chestPos = chestAreaList.get(randomIndex);
         world.setBlockState(chestPos, Blocks.CHEST.getDefaultState());
-        System.out.print("chestPos: " + chestPos + "\n");
     }
 }
