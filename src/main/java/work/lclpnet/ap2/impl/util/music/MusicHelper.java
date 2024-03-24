bfdcb7e6067ac065930a07ebbc4661c73a53e504
LCLP <lukas@lclpnet.work>
2024-03-24 18:52:12 +0100

    stop music and title animation if game is skipped

--- src/main/java/work/lclpnet/ap2/impl/util/music/MusicHelper.java
+++ src/main/java/work/lclpnet/ap2/impl/util/music/MusicHelper.java
@@ -5,8 +5,10 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import work.lclpnet.ap2.api.util.music.LoadableSong;
 import work.lclpnet.ap2.api.util.music.SongCache;
+import work.lclpnet.ap2.api.util.music.SongWrapper;
 import work.lclpnet.ap2.api.util.music.WeightedSong;
 import work.lclpnet.notica.Notica;
+import work.lclpnet.notica.api.SongHandle;
 
 import java.util.Collection;
 import java.util.Random;
@@ -17,25 +19,32 @@ public class MusicHelper {
 
     private MusicHelper() {}
 
-    public static void playSong(WeightedSong song, float volume, Collection<? extends ServerPlayerEntity> players,
-                                MinecraftServer server, SongCache cache, Logger logger) {
-        playSong(song, volume, players, server, cache, logger, random);
+    public static SongWrapper playSong(WeightedSong song, float volume, Collection<? extends ServerPlayerEntity> players,
+                                           MinecraftServer server, SongCache cache, Logger logger) {
+        return playSong(song, volume, players, server, cache, logger, random);
     }
 
-    public static void playSong(WeightedSong song, float volume, Collection<? extends ServerPlayerEntity> players,
-                                MinecraftServer server, SongCache cache, Logger logger, Random random) {
+    public static SongWrapper playSong(WeightedSong song, float volume, Collection<? extends ServerPlayerEntity> players,
+                                       MinecraftServer server, SongCache cache, Logger logger, Random random) {
 
+        SongWrapperImpl wrapper = new SongWrapperImpl();
         LoadableSong loadable = song.getRandomElement(random);
 
         loadable.load(cache, logger)
                 .thenAccept(config -> {
                     Notica notica = Notica.getInstance(server);
-                    notica.playSong(config.song(), volume * config.volume(), config.startTick(), players);
+
+                    float scaledVolume = volume * config.volume();
+                    SongHandle handle = notica.playSong(config.song(), scaledVolume, config.startTick(), players);
+
+                    wrapper.setHandle(handle);
                 })
                 .whenComplete((res, err) -> {
                     if (err == null) return;
 
                     logger.error("Failed to load song {}", loadable.getId());
                 });
+
+        return wrapper;
     }
 }
