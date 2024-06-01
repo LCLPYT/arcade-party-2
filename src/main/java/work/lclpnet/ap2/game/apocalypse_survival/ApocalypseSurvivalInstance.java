a31ebcd1ab0bc4e47c4bfdff74a01c54ba1f4a30
LCLP <lukas@lclpnet.work>
2024-06-01 19:51:33 +0200

    add ApocalypseSurvivalMiniGame

---
+++ src/main/java/work/lclpnet/ap2/game/apocalypse_survival/ApocalypseSurvivalInstance.java
@@ -0,0 +1,21 @@
+package work.lclpnet.ap2.game.apocalypse_survival;
+
+import work.lclpnet.ap2.api.game.MiniGameHandle;
+import work.lclpnet.ap2.impl.game.EliminationGameInstance;
+
+public class ApocalypseSurvivalInstance extends EliminationGameInstance {
+
+    public ApocalypseSurvivalInstance(MiniGameHandle gameHandle) {
+        super(gameHandle);
+    }
+
+    @Override
+    protected void prepare() {
+
+    }
+
+    @Override
+    protected void ready() {
+
+    }
+}
