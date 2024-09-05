79f7b0e29c0c5f9d349195a9feae8334c5f2a856
Till Nachtigall <tillnachtigall2@gmail.com>
2024-09-05 11:50:29 +0200

    add methods to store and get player progress in AimMasterManager

--- src/main/java/work/lclpnet/ap2/game/aim_master/AimMasterManager.java
+++ src/main/java/work/lclpnet/ap2/game/aim_master/AimMasterManager.java
@@ -2,10 +2,26 @@ package work.lclpnet.ap2.game.aim_master;
 
 import net.minecraft.server.world.ServerWorld;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class AimMasterManager {
+
+    private static Map<UUID, Integer> playerProgress;
+
     public AimMasterManager(Map<UUID, AimMasterDomain> domains, ServerWorld world) {
+
+        playerProgress = new HashMap<>();
+        List<UUID> players = new ArrayList<>(domains.size());
+
+        players.addAll(domains.keySet());
+
+        for (UUID player : players) {playerProgress.put(player, 0);}
     }
+
+    public void advancePlayer(UUID player) {
+        playerProgress.put(player, playerProgress.get(player) + 1);
+    }
+
+    public int getPlayerProgress(UUID player) {
+        return playerProgress.get(player);}
 }
