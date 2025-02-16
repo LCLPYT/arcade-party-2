badb35f821ac13e814520b0c1da56521e5261af5
LCLP <lukas@lclpnet.work>
2025-02-16 17:43:07 +0100

    make special items affected by gravity

--- src/main/java/work/lclpnet/ap2/impl/game/item/SpecialItemObject.java
+++ src/main/java/work/lclpnet/ap2/impl/game/item/SpecialItemObject.java
@@ -1,26 +1,40 @@
 package work.lclpnet.ap2.impl.game.item;
 
-import net.minecraft.entity.decoration.DisplayEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ModelTransformationMode;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Box;
 import work.lclpnet.ap2.impl.scene.ItemDisplayObject;
 import work.lclpnet.ap2.impl.scene.Object3d;
-import work.lclpnet.ap2.impl.scene.animation.Animatable;
-import work.lclpnet.ap2.impl.scene.animation.AnimationContext;
 
-public class SpecialItemObject extends Object3d implements Animatable {
+public class SpecialItemObject extends Object3d {
+
+    public static final double DEFAULT_SIZE = 0.25;
+    private final double size;
 
     public SpecialItemObject(ItemStack stack) {
+        this(stack, 0.25);
+    }
+
+    public SpecialItemObject(ItemStack stack, double size) {
+        this.size = size;
+
         ItemDisplayObject itemDisplay = new ItemDisplayObject(stack);
         itemDisplay.position.set(0, 0.25, 0);
+        itemDisplay.scale.set(size / DEFAULT_SIZE);
         itemDisplay.setTransformationMode(ModelTransformationMode.GROUND);
-        itemDisplay.setBillboardMode(DisplayEntity.BillboardMode.CENTER);
 
         addChild(itemDisplay);
     }
 
-    @Override
-    public void updateAnimation(double dt, AnimationContext ctx) {
+    public Box boxAt(double x, double y, double z) {
+        return new Box(
+                x - size, y, z - size,
+                x + size, y + 2 * size, z + size);
+    }
 
+    public boolean isOnGround(ServerWorld world) {
+        Box box = boxAt(position.x, position.y - 0.05, position.z);
+        return world.getBlockCollisions(null, box).iterator().hasNext();
     }
 }
--- src/main/java/work/lclpnet/ap2/impl/game/item/SpecialItemScene.java
+++ src/main/java/work/lclpnet/ap2/impl/game/item/SpecialItemScene.java
@@ -1,24 +1,62 @@
 package work.lclpnet.ap2.impl.game.item;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
+import org.joml.Vector3d;
 import work.lclpnet.ap2.impl.scene.MixedMountContext;
 import work.lclpnet.ap2.impl.scene.Scene;
+import work.lclpnet.ap2.impl.scene.animation.AnimationContext;
+import work.lclpnet.ap2.impl.scene.simulation.Gradient;
+import work.lclpnet.ap2.impl.scene.simulation.SimpleGravityGradient;
+import work.lclpnet.ap2.impl.scene.simulation.StateVector;
+import work.lclpnet.ap2.impl.scene.simulation.solver.EulerSolver;
+import work.lclpnet.ap2.impl.scene.simulation.solver.NumericalSolver;
 import work.lclpnet.ap2.impl.util.world.entity.DynamicEntityManager;
 import work.lclpnet.kibu.scheduler.api.TaskScheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.pow;
 
 public class SpecialItemScene {
 
     private final Scene scene;
+    private final List<SpecialItemObject> objects = new ArrayList<>();
+    private final Object2IntMap<SpecialItemObject> indices = new Object2IntOpenHashMap<>();
+    private final Gradient gravity = new SimpleGravityGradient(0.04 * pow(20, 2));
+    private final NumericalSolver solver = EulerSolver.INSTANCE;
+    private StateVector state = new StateVector(new Vector3d[0]);
 
     public SpecialItemScene(ServerWorld world) {
         var dynamicEntityManager = new DynamicEntityManager(world);
         this.scene = new Scene(new MixedMountContext(world, dynamicEntityManager));
+        indices.defaultReturnValue(-1);
     }
 
     public void init(TaskScheduler scheduler) {
         scene.animate(1, scheduler);
+        scene.onUpdateAnimation(this::updateSimulation);
+    }
+
+    private synchronized void updateSimulation(double dt, AnimationContext ctx) {
+        solver.solve(state, dt, gravity);
+
+        for (int i = 0; i < objects.size(); i++) {
+            SpecialItemObject obj = objects.get(i);
+
+            if (obj.isOnGround(ctx.world())) {
+                // reset velocity
+                state.getVector3(2 * i + 1).set(0);
+                continue;
+            }
+
+            obj.position.set(state.getVector3(2 * i));
+            obj.updateMatrixWorld();
+        }
     }
 
     public SpecialItemObject spawnItem(Vec3d pos, ItemStack stack) {
@@ -27,10 +65,52 @@ public class SpecialItemScene {
 
         scene.add(obj);
 
+        synchronized (this) {
+            indices.put(obj, objects.size());
+            objects.add(obj);
+
+            int size = state.size();
+            var vectors = new Vector3d[size + 2];
+
+            for (int i = 0; i < size; i++) {
+                vectors[i] = state.getVector3(i);
+            }
+
+            vectors[size] = new Vector3d(pos.x, pos.y, pos.z);
+            vectors[size + 1] = new Vector3d();
+
+            state = new StateVector(vectors);
+        }
+
         return obj;
     }
 
     public void remove(SpecialItemObject obj) {
         scene.remove(obj);
+
+        synchronized (this) {
+            int i = indices.removeInt(obj);
+
+            if (i == -1) return;
+
+            objects.remove(i);
+
+            for (int j = i; j < objects.size(); j++) {
+                indices.put(objects.get(j), j);
+            }
+
+            int size = state.size();
+            var vectors = new Vector3d[size - 2];
+
+            for (int j = 0; j < i; j++) {
+                vectors[j] = state.getVector3(j);
+            }
+
+            for (int j = i + 2; j < size; j++) {
+                vectors[j - 2] = state.getVector3(j);
+            }
+
+            state = new StateVector(vectors);
+        }
     }
 }
