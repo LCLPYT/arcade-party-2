b3b33fa054b0c1d57d567d412effab8fe92af234
LCLP <lukas@lclpnet.work>
2025-12-27 18:35:52 +0100

    add bye tracker data structure

---
+++ src/test/kotlin/work/lclpnet/ap2/game/pvp_tournament/tournament/ByeTrackerTest.kt
@@ -0,0 +1,57 @@
+package work.lclpnet.ap2.game.pvp_tournament.tournament
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ByeTrackerTest {
+
+    @Test
+    fun byeCountInTree() {
+        val bt = ByeTracker()
+
+        val match = Match().also { bt.register(it) }
+
+        assertEquals(0, bt.byeCountInTree(match))
+    }
+
+    @Test
+    fun addByeCountIncremented() {
+        val bt = ByeTracker()
+
+        val a = Match().also { bt.register(it) }
+        val b = Match().also { bt.register(it) }
+
+        bt.addBye(a)
+
+        assertEquals(1, bt.byeCountInTree(a))
+        assertEquals(0, bt.byeCountInTree(b))
+    }
+
+    @Test
+    fun mergeTreeSummed() {
+        val bt = ByeTracker()
+
+        val a = Match().also { bt.register(it) }
+        val b = Match().also { bt.register(it) }
+        val c = Match().also { bt.register(it) }
+        val d = Match().also { bt.register(it) }
+
+        bt.addBye(a)
+        bt.addBye(b)
+        bt.addBye(c)
+
+        bt.mergeTrees(a, b)
+
+        assertEquals(2, bt.byeCountInTree(a))
+        assertEquals(2, bt.byeCountInTree(b))
+
+        bt.mergeTrees(c, d)
+
+        assertEquals(1, bt.byeCountInTree(c))
+        assertEquals(1, bt.byeCountInTree(d))
+
+        bt.mergeTrees(b, d)
+
+        assertEquals(3, bt.byeCountInTree(a))
+    }
+}
