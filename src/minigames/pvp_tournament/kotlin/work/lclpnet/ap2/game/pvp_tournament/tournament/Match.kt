8b30ccf8434c1f1bd815b6a857f1cc450ea24adf
LCLP <lukas@lclpnet.work>
2025-12-29 19:37:03 +0100

    improve SingleEliminationTournamentBuilder

--- src/minigames/pvp_tournament/kotlin/work/lclpnet/ap2/game/pvp_tournament/tournament/Match.kt
+++ src/minigames/pvp_tournament/kotlin/work/lclpnet/ap2/game/pvp_tournament/tournament/Match.kt
@@ -3,12 +3,12 @@ package work.lclpnet.ap2.game.pvp_tournament.tournament
 import work.lclpnet.ap2.impl.game.data.type.PlayerRef
 
 class Match(
-    val leftChild: Match? = null,
-    val rightChild: Match? = null,
+    var leftChild: Match? = null,
+    var rightChild: Match? = null,
     var leftPlayer: PlayerRef? = null,
     var rightPlayer: PlayerRef? = null,
     var winnerNext: Match? = null,
-    var looserNext: Match? = null,
+    var loserNext: Match? = null,
 ) {
     var winner: PlayerRef? = null
         private set
@@ -25,10 +25,37 @@ class Match(
     }
 
     @Synchronized
-    fun acceptPlayerFromChildMatch(player: PlayerRef, from: Match) {
+    fun acceptPlayerFromChildMatch(player: PlayerRef?, from: Match) {
         when (from) {
             leftChild -> leftPlayer = player
             rightChild -> rightPlayer = player
         }
     }
+
+    fun isBye(): Boolean =
+        // no parent, and exactly one winner
+        (leftChild == null && rightChild == null && byeMatchWinner() != null)
+                // only left child and no right player
+                || (leftChild != null && rightChild == null && rightPlayer == null)
+                // only right child and no left player
+                || (leftChild == null && rightChild != null && leftPlayer == null)
+
+    fun byeMatchChild(): Match? = when {
+        leftChild != null && rightChild == null -> leftChild
+        leftChild == null && rightChild != null -> rightChild
+        else -> null
+    }
+
+    fun byeMatchWinner(): PlayerRef? = when {
+        leftPlayer != null && rightPlayer == null -> leftPlayer
+        leftPlayer == null && rightPlayer != null -> rightPlayer
+        else -> null
+    }
+
+    fun getChildren(): List<Match> = listOfNotNull(leftChild, rightChild)
+
+    fun isLeaf() =
+        leftChild == null && rightChild == null
+
+    fun isFinale() = winnerNext == null
 }
--- src/minigames/pvp_tournament/kotlin/work/lclpnet/ap2/game/pvp_tournament/tournament/Tournament.kt
+++ src/minigames/pvp_tournament/kotlin/work/lclpnet/ap2/game/pvp_tournament/tournament/Tournament.kt
@@ -2,10 +2,62 @@ package work.lclpnet.ap2.game.pvp_tournament.tournament
 
 import work.lclpnet.ap2.impl.game.data.type.PlayerRef
 
-class Tournament(
-    val matches: List<Match>,
-    val players: List<PlayerRef>,
-)
+data class Tournament(
+    val matches: Set<Match>,
+    val players: Set<PlayerRef>,
+) {
+    val finale: Match get() = matches.first { it.isFinale() }
+
+    fun simplified(): Tournament {
+        val newMatches = matches.toMutableSet()
+        val checkOptimize = newMatches.toMutableSet()
+
+        // eliminate bye matches where the winner is already known
+        while (checkOptimize.isNotEmpty()) {
+            val match = checkOptimize.first()
+            checkOptimize.remove(match)
+
+            if (!match.isBye()) continue
+
+            val parentMatch = match.winnerNext ?: continue
+            val winner = match.byeMatchWinner()
+
+            match.complete(winner)
+
+            newMatches.remove(match)
+
+            parentMatch.acceptPlayerFromChildMatch(winner, match)
+
+            val childMatch = match.byeMatchChild()
+
+            if (childMatch != null) {
+                when {
+                    childMatch.winnerNext == match -> {
+                        childMatch.winnerNext = parentMatch
+                    }
+                    childMatch.loserNext == match -> {
+                        childMatch.loserNext = parentMatch
+                    }
+                }
+            }
+
+            // remove child from parent, after accepting the player
+            when {
+                parentMatch.leftChild == match -> {
+                    parentMatch.leftChild = childMatch
+                }
+                parentMatch.rightChild == match -> {
+                    parentMatch.rightChild = childMatch
+                }
+            }
+
+            // propagate optimization
+            checkOptimize.add(parentMatch)
+        }
+
+        return Tournament(newMatches, players)
+    }
+}
 
 interface TournamentBuilder {
     fun build(players: List<PlayerRef>): Tournament
@@ -17,11 +69,12 @@ class SingleEliminationTournamentBuilder(
 
     override fun build(players: List<PlayerRef>): Tournament {
         val players = players.shuffled().toMutableList()
-        val matches = mutableListOf<Match>()
+        val matches = mutableSetOf<Match>()
 
         // start with a bye match for each player
         val initial = players.map { player -> Match(leftPlayer = player) }
 
+        matches.addAll(initial)
         initial.forEach { byeTracker.register(it) }
 
         var current = initial
@@ -34,7 +87,7 @@ class SingleEliminationTournamentBuilder(
             current = round
         } while (current.size > 1)
 
-        return Tournament(matches, players)
+        return Tournament(matches, players.toSet()).simplified()
     }
 
     fun buildRound(matches: List<Match>): List<Match> {
@@ -46,21 +99,27 @@ class SingleEliminationTournamentBuilder(
             pool.remove(match)
 
             val byeMatch = Match(leftChild = match)
-            byeTracker.register(byeMatch)
-            byeTracker.mergeTrees(match, byeMatch)
-            byeTracker.addBye(byeMatch)
+
+            match.winnerNext = byeMatch
 
             round.add(byeMatch)
+            byeTracker.register(byeMatch)
+
+            byeTracker.mergeTrees(match, byeMatch)
+            byeTracker.addBye(byeMatch)
         }
 
-        pool.chunked(2).map {
-            Match(
+        pool.chunked(2).forEach {
+            val match = Match(
                 leftChild = it[0],
                 rightChild = it[1],
             )
-        }.forEach {
-            round.add(it)
-            byeTracker.register(it)
+
+            it[0].winnerNext = match
+            it[1].winnerNext = match
+
+            round.add(match)
+            byeTracker.register(match)
         }
 
         return round
