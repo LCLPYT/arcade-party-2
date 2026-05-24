6afdc1291b36fd34869a38b53ad0e44e752a5c4d
LCLP <lukas@lclpnet.work>
2026-05-24 23:12:41 +0200

    feat: add weapon swap minigame

---
+++ src/minigames/weapon_swap/src/main/kotlin/work/lclpnet/ap2/game/weapon_swap/WeaponSwapMiniGame.kt
@@ -0,0 +1,18 @@
+package work.lclpnet.ap2.game.weapon_swap
+
+import net.minecraft.core.RegistryAccess
+import net.minecraft.resources.Identifier
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import work.lclpnet.ap2.ApConstants
+import work.lclpnet.ap2.api.game.*
+
+class WeaponSwapMiniGame : MiniGame {
+    override fun getId(): Identifier = ApConstants.identifier("weapon_swap")
+    override fun getType(): GameType = GameType.FFA
+    override fun getAuthor(): String = ApConstants.PERSON_LCLP
+    override fun getIcon(manager: RegistryAccess): ItemStack = ItemStack(Items.WOODEN_SWORD)
+    override fun canBeFinale(context: GameStartContext): Boolean = true
+    override fun canBePlayed(context: GameStartContext): Boolean = true
+    override fun createInstance(gameHandle: MiniGameHandle): MiniGameInstance = WeaponSwapInstance(gameHandle)
+}
