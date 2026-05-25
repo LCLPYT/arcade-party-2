abe57e5ef55f93ce048e7174b96bbef4c6a962b0
LCLP <lukas@lclpnet.work>
2026-05-26 00:25:48 +0200

    fix: clicking answers in guess it

--- src/minigames/guess_it/src/main/java/work/lclpnet/ap2/game/guess_it/data/InputManager.java
+++ src/minigames/guess_it/src/main/java/work/lclpnet/ap2/game/guess_it/data/InputManager.java
@@ -1,9 +1,11 @@
 package work.lclpnet.ap2.game.guess_it.data;
 
 import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -12,6 +14,7 @@ import work.lclpnet.ap2.api.base.Participants;
 import work.lclpnet.kibu.access.entity.ServerPlayerAccess;
 import work.lclpnet.kibu.hook.HookRegistrar;
 import work.lclpnet.kibu.hook.ServerMessageHooks;
+import work.lclpnet.kibu.hook.network.CustomClickActionCallback;
 import work.lclpnet.kibu.translate.Translations;
 import work.lclpnet.kibu.translate.text.TranslatedText;
 
@@ -24,21 +27,31 @@ public class InputManager implements InputInterface {
     private final Translations translations;
     private final Participants participants;
     private final ChallengeMessenger messenger;
+    private final Identifier answerId;
     private InputValue inputValue = null;
     private OptionValue optionValue = null;
     private boolean locked = false;
 
-    public InputManager(PlayerChoices choices, Translations translations, Participants participants, ChallengeMessenger messenger) {
+    public InputManager(PlayerChoices choices, Translations translations, Participants participants, ChallengeMessenger messenger, Identifier answerId) {
         this.choices = choices;
         this.translations = translations;
         this.participants = participants;
         this.messenger = messenger;
+        this.answerId = answerId;
     }
 
     public void init(HookRegistrar hooks) {
         ServerMessageHooks.ALLOW_CHAT_MESSAGE.registerWith(hooks, (message, sender, params) -> {
             onChat(message, sender, params);
             return false;
+        });
+
+        CustomClickActionCallback.HOOK.registerWith(hooks, (player, id, payload) -> {
+            if (!id.equals(answerId)) return;
+
+            if (payload.orElse(null) instanceof StringTag(String value)) {
+                input(player, value);
+            }
         });
     }
 
