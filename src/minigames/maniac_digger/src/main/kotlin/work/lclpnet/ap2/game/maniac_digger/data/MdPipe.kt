65c81d36e1f52cf36f75813c2bf12de220bda495
LCLP <lukas@lclpnet.work>
2026-05-28 17:48:27 +0200

    feat: add MdPipePath to better match pipe progress

--- src/minigames/maniac_digger/src/main/kotlin/work/lclpnet/ap2/game/maniac_digger/data/MdPipe.kt
+++ src/minigames/maniac_digger/src/main/kotlin/work/lclpnet/ap2/game/maniac_digger/data/MdPipe.kt
@@ -3,4 +3,4 @@ package work.lclpnet.ap2.game.maniac_digger.data
 import net.minecraft.world.phys.Vec3
 import work.lclpnet.gaco.ds.BlockBox
 
-data class MdPipe(val spawn: Vec3, val bounds: BlockBox)
+data class MdPipe(val spawn: Vec3, val bounds: BlockBox, val path: MdPipePath)
