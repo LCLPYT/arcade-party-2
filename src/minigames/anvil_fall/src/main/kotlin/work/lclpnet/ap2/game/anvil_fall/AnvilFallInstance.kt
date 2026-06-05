64f6a6a0a88cfc0a39edb797baedf0994b93a618
LCLP <lukas@lclpnet.work>
2026-06-06 01:57:18 +0200

    feat: add stats to anvil fall

--- src/minigames/anvil_fall/src/main/kotlin/work/lclpnet/ap2/game/anvil_fall/AnvilFallInstance.kt
+++ src/minigames/anvil_fall/src/main/kotlin/work/lclpnet/ap2/game/anvil_fall/AnvilFallInstance.kt
@@ -19,6 +19,7 @@ import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.Vec3
 import net.minecraft.world.scores.Team
 import work.lclpnet.ap2.api.game.MiniGameHandle
+import work.lclpnet.ap2.api.stats.CommonStats
 import work.lclpnet.ap2.ext.mc.isOf
 import work.lclpnet.ap2.ext.mc.playNotifySound
 import work.lclpnet.ap2.ext.runEveryTick
@@ -33,10 +34,12 @@ import work.lclpnet.game.impl.prot.ProtectionTypes
 import work.lclpnet.kibu.access.VelocityModifier
 import work.lclpnet.kibu.access.entity.FallingBlockAccess
 import work.lclpnet.kibu.hook.player.PlayerMoveCallback
+import work.lclpnet.kibu.hook.util.PositionRotation
 import work.lclpnet.kibu.scheduler.Ticks
 import work.lclpnet.kibu.translate.bossbar.TranslatedBossBar
 import work.lclpnet.kibu.translate.text.FormatWrapper
 import java.util.*
+import kotlin.math.sqrt
 
 const val DIRECT_ANVIL_CHANCE = 0.02
 const val SPREAD_RADIUS = 8
@@ -52,11 +55,14 @@ class AnvilFallInstance(gameHandle: MiniGameHandle) : EliminationGameInstance(ga
     private lateinit var setup: AnvilFallSetup
     private var playArea: BlockBox? = null
     private lateinit var center: Vec3
+    private val stats = createStats(CommonStats.DistanceMoved, CommonStats.TimeSurvived)
 
     override fun prepare() {
         commons().gameRuleBuilder()
             .set(GameRules.ENTITY_DROPS, false)
             .set(GameRules.FALL_DAMAGE, true)
+
+        trackSurvivalTime(stats)
 
         scanWorld()
 
@@ -84,7 +90,8 @@ class AnvilFallInstance(gameHandle: MiniGameHandle) : EliminationGameInstance(ga
         setupBossBar()
         startAnvilSpawning()
 
-        PlayerMoveCallback.HOOK.registerWith(gameHandle.hooks) { player, _, to ->
+        PlayerMoveCallback.HOOK.registerWith(gameHandle.hooks) { player, from, to ->
+            trackDistance(player, from, to)
             repelPlayer(player, to)
             false
         }
@@ -201,6 +208,15 @@ class AnvilFallInstance(gameHandle: MiniGameHandle) : EliminationGameInstance(ga
         FallingBlockAccess.setBlockState(anvil, state)
 
         world.addFreshEntity(anvil)
+    }
+
+    private fun trackDistance(player: ServerPlayer, from: PositionRotation, to: PositionRotation) {
+        if (!gameHandle.participants.isParticipating(player)) return
+
+        val dx = to.x() - from.x()
+        val dz = to.z() - from.z()
+
+        stats.modify(player, CommonStats.DistanceMoved) { it + sqrt(dx * dx + dz * dz).toFloat() }
     }
 
     private fun repelPlayer(player: ServerPlayer, to: Position) {
