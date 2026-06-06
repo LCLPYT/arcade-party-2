40471b39fc5f742fb209f49441e95cb6f9a143f0
LCLP <lukas@lclpnet.work>
2026-06-06 17:59:22 +0200

    feat: add KnockbackKillTracker to track eliminations

---
+++ src/lib/src/main/kotlin/work/lclpnet/ap2/impl/util/world/KnockbackKillTracker.kt
@@ -0,0 +1,66 @@
+package work.lclpnet.ap2.impl.util.world
+
+import net.minecraft.server.level.ServerPlayer
+import work.lclpnet.ap2.ext.inWholeTicks
+import work.lclpnet.ap2.game.player.Participants
+import work.lclpnet.ap2.impl.util.DeathMessages
+import work.lclpnet.kibu.hook.util.OnGroundDetector
+import work.lclpnet.kibu.scheduler.api.TaskScheduler
+import work.lclpnet.kibu.translate.text.TranslatedText
+import java.util.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+class KnockbackKillTracker(
+    private val participants: Participants,
+    private val trackerTimeout: Duration = 10.seconds,
+) {
+
+    private val entries = HashMap<UUID, Entry>()
+
+    fun init(scheduler: TaskScheduler) {
+        scheduler.interval(1) { -> tick() }
+    }
+
+    fun onHit(victim: ServerPlayer, attacker: ServerPlayer) {
+        if (victim.uuid == attacker.uuid) return
+        entries[victim.uuid] = Entry(attacker.uuid, trackerTimeout.inWholeTicks.toInt())
+    }
+
+    fun getKiller(victim: ServerPlayer): ServerPlayer? {
+        val entry = entries[victim.uuid] ?: return null
+        return victim.level().server.playerList.getPlayer(entry.attacker)
+    }
+
+    fun killMessage(victim: ServerPlayer, deathMessages: DeathMessages): TranslatedText? {
+        val killer = getKiller(victim) ?: return null
+        return deathMessages.killedBy(victim, killer)
+    }
+
+    private fun tick() {
+        val it = entries.entries.iterator()
+
+        while (it.hasNext()) {
+            val (uuid, entry) = it.next()
+            val player = participants.getParticipant(uuid).orElse(null)
+
+            if (player == null) {
+                it.remove()
+                continue
+            }
+
+            if (!OnGroundDetector.isOnGroundServer(player)) {
+                entry.airborne = true
+                continue
+            }
+
+            if (entry.airborne || --entry.timeout <= 0) {
+                it.remove()
+            }
+        }
+    }
+
+    private class Entry(val attacker: UUID, var timeout: Int) {
+        var airborne = false
+    }
+}
