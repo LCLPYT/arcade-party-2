2d483ebc0dabf31cc31d579b4a877c9085a399c2
LCLP <lukas@lclpnet.work>
2026-06-08 14:27:54 +0200

    feat: add RapidRunner setup

---
+++ src/minigames/rapid_runner/src/main/kotlin/work/lclpnet/ap2/rapid_runner/RapidRunnerMinigame.kt
@@ -0,0 +1,18 @@
+package work.lclpnet.ap2.rapid_runner
+
+import net.minecraft.core.RegistryAccess
+import net.minecraft.resources.Identifier
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import work.lclpnet.ap2.ApConstants
+import work.lclpnet.ap2.api.game.*
+
+class RapidRunnerMinigame : MiniGame {
+    override fun getId(): Identifier = ApConstants.identifier("rapid_runner")
+    override fun getType(): GameType = GameType.FFA
+    override fun getAuthor(): String = ApConstants.PERSON_LCLP
+    override fun getIcon(manager: RegistryAccess): ItemStack = ItemStack(Items.COPPER_BOOTS)
+    override fun canBeFinale(context: GameStartContext): Boolean = true
+    override fun canBePlayed(context: GameStartContext): Boolean = true
+    override fun createInstance(gameHandle: MiniGameHandle): MiniGameInstance = RapidRunnerInstance(gameHandle)
+}
