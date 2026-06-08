35f2e247c1098f4edc667496e77b5fe7883d61c7
LCLP <lukas@lclpnet.work>
2026-06-08 17:20:47 +0200

    refactor(game-api): migrate aim master to new factory paradigm

--- src/minigames/aim_master/src/main/kotlin/work/lclpnet/ap2/game/aim_master/AimMasterMiniGame.kt
+++ src/minigames/aim_master/src/main/kotlin/work/lclpnet/ap2/game/aim_master/AimMasterMiniGame.kt
@@ -6,7 +6,6 @@ import net.minecraft.world.item.Items
 import work.lclpnet.ap2.ApConstants
 import work.lclpnet.ap2.api.game.GameStartContext
 import work.lclpnet.ap2.api.game.GameType
-import work.lclpnet.ap2.game.MapLevelGameFactory
 import work.lclpnet.ap2.game.MiniGame
 import work.lclpnet.ap2.game.MiniGameHandle
 
@@ -17,5 +16,5 @@ class AimMasterMiniGame : MiniGame {
     override fun getIcon(manager: RegistryAccess) = ItemStack(Items.TARGET)
     override fun canBeFinale(context: GameStartContext) = true
     override fun canBePlayed(context: GameStartContext) = true
-    override fun createFactory(gameHandle: MiniGameHandle) = MapLevelGameFactory(::AimMasterInstance)
+    override fun createFactory(gameHandle: MiniGameHandle) = AimMasterFactory()
 }
