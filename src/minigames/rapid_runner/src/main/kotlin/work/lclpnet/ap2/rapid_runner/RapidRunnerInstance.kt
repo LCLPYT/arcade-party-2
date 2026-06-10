9a8030c33d716bffc342380381d49b09c74404ea
LCLP <lukas@lclpnet.work>
2026-06-10 21:49:23 +0200

    feat: temporary overworld level generation

--- src/minigames/rapid_runner/src/main/kotlin/work/lclpnet/ap2/rapid_runner/RapidRunnerInstance.kt
+++ src/minigames/rapid_runner/src/main/kotlin/work/lclpnet/ap2/rapid_runner/RapidRunnerInstance.kt
@@ -1,19 +1,24 @@
 package work.lclpnet.ap2.rapid_runner
 
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup
 import net.minecraft.server.level.ServerLevel
+import work.lclpnet.ap2.ext.mc.teleport
 import work.lclpnet.ap2.game.MiniGameHandle
 import work.lclpnet.ap2.game.MiniGameInstance
 import work.lclpnet.ap2.game.player.ParticipantListener
-import work.lclpnet.game.map.GameMap
 import work.lclpnet.game.util.ProtectorUtils
 
-class RapidRunnerInstance(val gameHandle: MiniGameHandle, level: ServerLevel, map: GameMap) : MiniGameInstance {
+class RapidRunnerInstance(val gameHandle: MiniGameHandle, val level: ServerLevel) : MiniGameInstance {
 
     override fun start() {
         gameHandle.protect { config ->
             config.disallowAll()
 
             ProtectorUtils.allowCreativeOperatorBypass(config)
+        }
+
+        for (player in PlayerLookup.all(gameHandle.server)) {
+            player.teleport(level.respawnData.globalPos.pos, level)
         }
     }
 
