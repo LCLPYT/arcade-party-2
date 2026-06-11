c85fed1db2f8ee43cefa00b78c214e83df9e255a
LCLP <lukas@lclpnet.work>
2026-06-11 15:56:35 +0200

    feat: add changeMap to MapFacade (originally from mg-api WorldFacade)

--- src/lib/src/main/java/work/lclpnet/ap2/api/map/MapFacade.java
+++ src/lib/src/main/java/work/lclpnet/ap2/api/map/MapFacade.java
@@ -6,7 +6,7 @@ import net.minecraft.server.level.ServerLevel;
 import org.jetbrains.annotations.Nullable;
 import work.lclpnet.ap2.api.game.MapReady;
 import work.lclpnet.gaco.asset.AssetRepository;
-import work.lclpnet.game.api.MapOptions;
+import work.lclpnet.game.api.WorldOptions;
 import work.lclpnet.game.map.GameMap;
 
 import java.util.List;
@@ -16,15 +16,26 @@ import java.util.concurrent.CompletableFuture;
 public interface MapFacade {
 
     /**
+     * Changes the current map.
+     * If the new map is not yet loaded, it will be loaded first.
+     * All players will be moved to the new map by default.
+     * Newly joining players will be moved to the new map as well.
+     * @param identifier The map id.
+     * @param options The world options to specify loading behavior.
+     * @return A future of the loaded map level.
+     */
+    CompletableFuture<ServerLevel> changeMap(Identifier identifier, WorldOptions options);
+
+    /**
      * Opens a random map that matches a given game identifier.
      * For example, if <code>ap:spleef</code> is given, this method will open a random map for the "spleef" mini-game.
      * @param gameId The game identifier.
      * @param mapOptions The map options.
      * @return A future that completes if the map was opened.
      */
-    CompletableFuture<Pair<ServerLevel, GameMap>> openRandomMap(Identifier gameId, MapOptions mapOptions);
+    CompletableFuture<Pair<ServerLevel, GameMap>> openRandomMap(Identifier gameId, WorldOptions mapOptions);
 
-    void openRandomMap(Identifier gameId, MapOptions options, MapReady onReady);
+    void openRandomMap(Identifier gameId, WorldOptions options, MapReady onReady);
 
     CompletableFuture<List<Identifier>> getMapIds(Identifier gameId);
 
@@ -43,7 +54,7 @@ public interface MapFacade {
     }
 
     default void openRandomMap(Identifier gameId, MapReady onReady) {
-        openRandomMap(gameId, MapOptions.TEMPORARY, onReady);
+        openRandomMap(gameId, WorldOptions.TEMPORARY, onReady);
     }
 
     /**
@@ -53,6 +64,6 @@ public interface MapFacade {
      * @return A future that completes if the map was opened.
      */
     default CompletableFuture<Pair<ServerLevel, GameMap>> openRandomMap(Identifier gameId) {
-        return openRandomMap(gameId, MapOptions.TEMPORARY);
+        return openRandomMap(gameId, WorldOptions.TEMPORARY);
     }
 }
--- src/lib/src/main/kotlin/work/lclpnet/ap2/game/util/MapLevelGameFactory.kt
+++ src/lib/src/main/kotlin/work/lclpnet/ap2/game/util/MapLevelGameFactory.kt
@@ -7,7 +7,7 @@ import work.lclpnet.ap2.game.MiniGameFactory
 import work.lclpnet.ap2.game.MiniGameHandle
 import work.lclpnet.ap2.game.MiniGameInstance
 import work.lclpnet.ap2.impl.map.schema.MapSchemaLoader
-import work.lclpnet.game.api.MapOptions
+import work.lclpnet.game.api.WorldOptions
 import work.lclpnet.game.map.GameMap
 import work.lclpnet.map_api.GameMapApi
 import kotlin.coroutines.resume
@@ -15,7 +15,7 @@ import kotlin.coroutines.resumeWithException
 
 suspend fun MiniGameHandle.openRandomMap(): Pair<ServerLevel, GameMap> {
     return suspendCancellableCoroutine { continuation ->
-        val options = object : MapOptions {
+        val options = object : WorldOptions {
             override fun shouldBeDeleted() = true
             override fun isCleanMapRequired() = true
             override fun shouldTeleportPlayers() = false
