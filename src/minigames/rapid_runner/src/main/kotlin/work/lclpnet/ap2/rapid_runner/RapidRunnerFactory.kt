3d6f416aefcb6422d7fd11cadf171ab35215a80f
LCLP <lukas@lclpnet.work>
2026-06-11 22:35:52 +0200

    chore: migrate to new map api and use world facade for rapid runner

--- src/minigames/rapid_runner/src/main/kotlin/work/lclpnet/ap2/rapid_runner/RapidRunnerFactory.kt
+++ src/minigames/rapid_runner/src/main/kotlin/work/lclpnet/ap2/rapid_runner/RapidRunnerFactory.kt
@@ -1,5 +1,6 @@
 package work.lclpnet.ap2.rapid_runner
 
+import kotlinx.coroutines.future.await
 import net.minecraft.core.registries.Registries
 import net.minecraft.server.MinecraftServer
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource
@@ -10,25 +11,39 @@ import net.minecraft.world.level.levelgen.NoiseGeneratorSettings
 import work.lclpnet.ap2.game.MiniGameFactory
 import work.lclpnet.ap2.game.MiniGameHandle
 import work.lclpnet.ap2.game.MiniGameInstance
+import work.lclpnet.game.api.WorldOptions
+import work.lclpnet.kibu.hook.util.PositionRotation
 import xyz.nucleoid.fantasy.Fantasy
 import xyz.nucleoid.fantasy.RuntimeLevelConfig
+import java.util.concurrent.CompletableFuture
 import kotlin.random.Random
 
 class RapidRunnerFactory : MiniGameFactory {
 
     override suspend fun createInstance(handle: MiniGameHandle): MiniGameInstance {
-        val generator = overworldGenerator(handle.server)
+        val level = handle.worldFacade.changeLevel(
+            handle.gameInfo.identifier("overworld"),
+            WorldOptions.TEMPORARY,
+            { _ ->
+                CompletableFuture.completedFuture(PositionRotation(0.0, 0.0, 0.0, 0f, 0f))
+            },
+            { key ->
+                val generator = overworldGenerator(handle.server)
 
-        val config = RuntimeLevelConfig()
-            .setSeed(Random.nextLong())
-            .setDimensionType(BuiltinDimensionTypes.OVERWORLD)
-            .setGenerator(generator)
-            .setShouldTickTime(true)
+                val config = RuntimeLevelConfig()
+                    .setSeed(Random.nextLong())
+                    .setDimensionType(BuiltinDimensionTypes.OVERWORLD)
+                    .setGenerator(generator)
+                    .setShouldTickTime(true)
 
-        val levelHandle = Fantasy.get(handle.server)
-            .openTemporaryLevel(handle.gameInfo.identifier("overworld"), config)
+                val levelHandle = Fantasy.get(handle.server)
+                    .openTemporaryLevel(key.identifier(), config)
 
-        return RapidRunnerInstance(handle, levelHandle.asLevel())
+                CompletableFuture.completedFuture(levelHandle)
+            }
+        ).await()
+
+        return RapidRunnerInstance(handle, level)
     }
 
     private fun overworldGenerator(server: MinecraftServer): NoiseBasedChunkGenerator {
