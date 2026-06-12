cf25af032bd6b273d6ef1d277d05912119de6e5d
LCLP <lukas@lclpnet.work>
2026-06-13 00:46:40 +0200

    fix: game end in weapon swap now lets the actually remaining players win

--- src/minigames/weapon_swap/src/main/kotlin/work/lclpnet/ap2/game/weapon_swap/WeaponSwapInstance.kt
+++ src/minigames/weapon_swap/src/main/kotlin/work/lclpnet/ap2/game/weapon_swap/WeaponSwapInstance.kt
@@ -39,7 +39,7 @@ private const val TWO_WEAPONS_THRESHOLD = 8
 private const val SPAWN_SPACING_DEFAULT = 8.0
 private val MIN_SWAP_DELAY = 7.seconds
 private val MAX_SWAP_DELAY = 10.seconds
-private val DRAW_DELAY = 3.minutes
+private val DRAW_DELAY = 3.minutes + 30.seconds
 private val WARN_BEFORE_END_DELAY = 30.seconds
 const val MOVEMENT_SPEED = 0.14f
 
@@ -113,6 +113,7 @@ class WeaponSwapInstance(
             val subject = gameHandle.translations.translateText("game.ap2.weapon_swap.end")
 
             commons().createTimer(subject, WARN_BEFORE_END_DELAY.inWholeSeconds.toInt()).whenDone {
+                data.addAll(players())
                 winManager.complete()
             }
         }
