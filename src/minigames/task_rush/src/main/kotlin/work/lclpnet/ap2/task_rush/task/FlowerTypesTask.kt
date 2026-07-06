2be5929b1ff95c813643b31652042e5200e75497
LCLP <lukas@lclpnet.work>
2026-07-06 21:19:24 +0200

    feat: remove previous flowers from the player inventory for the flower collection task

--- src/minigames/task_rush/src/main/kotlin/work/lclpnet/ap2/task_rush/task/FlowerTypesTask.kt
+++ src/minigames/task_rush/src/main/kotlin/work/lclpnet/ap2/task_rush/task/FlowerTypesTask.kt
@@ -2,6 +2,7 @@ package work.lclpnet.ap2.task_rush.task
 
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 import work.lclpnet.ap2.ext.mc.isIn
 import work.lclpnet.ap2.game.data.IntScoreDataContainer
 import work.lclpnet.ap2.game.data.Ordering
@@ -24,7 +25,7 @@ object FlowerTypesTask : Task {
 
         for (i in 0 until inventory.containerSize) {
             val stack = inventory.getItem(i)
-            if (!stack.isEmpty && stack.isIn(ApItemTags.FLOWERS)) {
+            if (isFlower(stack)) {
                 types.add(stack.item)
             }
         }
@@ -32,11 +33,17 @@ object FlowerTypesTask : Task {
         return types
     }
 
+    private fun isFlower(stack: ItemStack): Boolean =
+        stack.isIn(ApItemTags.FLOWERS)
+
     override fun begin(env: TaskEnv) {
         val startTypes = HashMap<UUID, Set<Item>>()
+        val removed = mutableMapOf<UUID, List<ItemStack>>()
 
         for (player in env.players) {
             startTypes[player.uuid] = flowerTypes(player)
+
+            removed[player.uuid] = removeItems(player)
         }
 
         env.timer("task.$id.task", 45.seconds) {
@@ -46,9 +53,27 @@ object FlowerTypesTask : Task {
                 val start = startTypes[player.uuid] ?: emptySet()
                 val gained = flowerTypes(player).count { it !in start }
                 data.setScore(player, gained)
+
+                removed[player.uuid]?.forEach { env.give(player, it) }
             }
 
             env.complete(data)
         }
+    }
+
+    private fun removeItems(player: ServerPlayer): List<ItemStack> {
+        val removed = ArrayList<ItemStack>()
+        val inventory = player.inventory
+
+        for (i in 0 until inventory.containerSize) {
+            val stack = inventory.getItem(i)
+
+            if (isFlower(stack)) {
+                removed.add(stack.copy())
+                inventory.setItem(i, ItemStack.EMPTY)
+            }
+        }
+
+        return removed
     }
 }
