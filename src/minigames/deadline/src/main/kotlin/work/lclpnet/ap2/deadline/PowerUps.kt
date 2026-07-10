2b72492f654ab50cee17d71baae3cdc7f2e2593e
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2026-07-10 20:14:24 +0200

    feat: remove the power-up refresh boss bar

--- src/minigames/deadline/src/main/kotlin/work/lclpnet/ap2/deadline/PowerUps.kt
+++ src/minigames/deadline/src/main/kotlin/work/lclpnet/ap2/deadline/PowerUps.kt
@@ -1,17 +1,14 @@
 package work.lclpnet.ap2.deadline
 
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.sounds.SoundEvents
-import net.minecraft.world.BossEvent
 import work.lclpnet.ap2.ext.inWholeTicks
 import work.lclpnet.ap2.game.MiniGameHandle
 import work.lclpnet.ap2.impl.game.item.SpecialItemObject
 import work.lclpnet.ap2.impl.game.item.SpecialItems
 import work.lclpnet.ap2.impl.util.debug.DebugController
 import work.lclpnet.game.map.GameMap
-import work.lclpnet.game.util.BossBarTimer
 import work.lclpnet.kibu.access.entity.PlayerInventoryAccess
 import work.lclpnet.kibu.hook.player.PlayerInventoryHooks
 import java.util.Random
@@ -66,25 +63,12 @@ class PowerUps(
     }
 
     fun startRefreshing(positions: List<BlockPos>) {
-        val subject = gameHandle.translations.translateText("power_up.refresh")
-
-        val timer = BossBarTimer.builder(gameHandle.translations, subject)
-            .withAlertSound(false)
-            .withColor(BossEvent.BossBarColor.YELLOW)
-            .withDurationTicks(REFRESH_INTERVAL.inWholeTicks.toInt())
-            .build()
-
-        timer.addPlayers(PlayerLookup.all(gameHandle.server))
-        timer.start(gameHandle.bossBarProvider, gameHandle.scheduler)
-
-        timer.whenDone {
+        gameHandle.scheduler.interval(REFRESH_INTERVAL.inWholeTicks) { ->
             spawn(positions)
 
             for (player in gameHandle.participants) {
                 REFRESH_SOUND.playTo(player)
             }
-
-            startRefreshing(positions)
         }
     }
 
